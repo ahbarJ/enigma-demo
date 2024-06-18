9fd08632ad9b47514a24f4892910cffd9188696f
Akbar <ahbarurmia@gmail.com>
2024-06-18 20:27:02 +0330

    quotes

---
+++ app/src/main/java/com/ahbarx/enigmademo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.ahbarx.enigmademo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv1, tv2, tv3, tv4;
+    String[] texts = {
+            "“Be yourself; everyone else is already taken.”\n" +
+            "― Oscar Wilde",
+            "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”\n" +
+            "― Albert Einstein",
+            "“Be the change that you wish to see in the world.”\n" +
+                    "― Mahatma Gandhi",
+            "“If you tell the truth, you don't have to remember anything.”\n" +
+                    "― Mark Twain"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tv1.setText(texts[0]);
+        tv2.setText(texts[1]);
+        tv3.setText(texts[2]);
+        tv4.setText(texts[3]);
+    }
+}
